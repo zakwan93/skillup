b900302e866c0de386cd294a0860da9b80ace4ae
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-05-08 17:07:30 -0400

    Add friendly_id gem to the project

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ gem "haml-rails", "~> 2.0"
 gem 'simple_form'
 gem 'faker'
 gem 'devise'
+gem 'friendly_id', '~> 5.4.0'
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,16 @@
 class Course < ApplicationRecord
     validates_presence_of :title, :description
+
     has_rich_text :description
+
     belongs_to :user
+
+    extend FriendlyId
+    # friendly_id :title, use: :slugged
+
+    friendly_id :generated_slug, use: :slugged
+    def generated_slug
+        require 'securerandom'
+        @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
+    end
 end
