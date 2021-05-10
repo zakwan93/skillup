e39361a82bdce73102c25286d499b59eba0bc67e
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-05-10 02:03:28 -0400

    fixing heroku seed error

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Set the default host and port to be the same as Action Mailer.
-MyApp::Application.default_url_options = MyApp::Application.config.action_mailer.default_url_options
