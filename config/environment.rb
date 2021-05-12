efeb4fcfd1f4ca49aada1eca54f415564f414536
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-05-12 02:17:01 -0400

    changed domain

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,8 @@ ActionMailer::Base.smtp_settings = {
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'skillzup.heroku.com',
     :authentication => :plain,
   } 
+
+  ActionMailer::Base.delivery_method = :smtp
