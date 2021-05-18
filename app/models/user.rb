85d4e8d298737ac96a7b6dda0e3a02c50a94f1b2
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-05-18 17:12:56 -0400

    Added gem rolify to user and added some role to new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
-  has_many :courses
 
+  rolify
+
+  
+  has_many :courses
+  
   def username
     self.email.split(/@/).first
   end
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher) 
+      self.add_role(:student) 
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher)
+    end
+  end
+
 end
