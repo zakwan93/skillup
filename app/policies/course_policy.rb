e6121ce3075a48cf75a2ede259ffbd6d450a439e
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-05-19 15:59:31 -0400

    Added user policy and some valifdation to perform action in user and course table using pundit

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -6,6 +6,22 @@ class CoursePolicy < ApplicationPolicy
   end
 
   def edit?
-    @user.has_role?:admin
+    @record.user == @user
+  end
+
+  def update?
+    @record.user == @user
+  end
+
+  def new?
+    @user.has_role?(:teacher)
+  end
+
+  def create?
+    @user.has_role?(:teacher)
+  end
+
+  def destroy?
+    @user.has_role?(:admin) || @record.user_id == @user.id
   end
 end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,16 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  
+  def edit?
+    @user.has_role?:admin
+  end
+
+  def update?
+    @user.has_role?:admin
+  end
+  
+end
